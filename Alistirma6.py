78e57a223ad4b67c574221fb6f85d3c809d1609d
asudesagirr <56042663+asudesagirr@users.noreply.github.com>
2019-10-16 00:04:15 +0300

    Update Alistirma6.py

--- Alistirma6.py
+++ Alistirma6.py
@@ -3,9 +3,11 @@
 sayac=0
 
 for i in range(1,10):
-    for j in range(0,10):
-        if (i>j):
-            sayac=sayac+1
+    for k in range(0,10):
+        for j in range(0,10):
+            for m in range(0,10):
+                if (i>m):
+                  sayac=sayac+1
 
 
 

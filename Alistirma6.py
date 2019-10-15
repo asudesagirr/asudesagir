d19a9f695d5d804006e783bb78e7177d2c7cf8fb
asudesagirr <56042663+asudesagirr@users.noreply.github.com>
2019-10-15 23:52:02 +0300

    Add files via upload

---
+++ Alistirma6.py
@@ -0,0 +1,12 @@
+#4 basamaklı sayılardan kaç adedinin ilk basamağı son basamağından büyüktür?
+
+sayac=0
+
+for i in range(1,10):
+    for j in range(0,10):
+        if (i>j):
+            sayac=sayac+1
+
+
+
+print(sayac)            
